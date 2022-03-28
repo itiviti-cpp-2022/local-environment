b1bb1971c7bdc9b5187e36f4c5e48576348dbc62
renbou <artemmikheev1311@gmail.com>
2022-03-28 20:49:22 +0300

    fix: containers not being removed after run

--- internal/util/docker.py
+++ internal/util/docker.py
@@ -6,7 +6,7 @@ from .template import format_template
 
 def run_docker(path, act, volumes=[]):
   return run_with_stdout(format_template(
-      "docker run --volume={{path}}:{{container_repo_path}} " +
+      "docker run --rm --volume={{path}}:{{container_repo_path}} " +
       "".join(["--volume=" + v + " " for v in volumes]) +
       "cpp-env:1.0 {{act}}",
       {**variables, "act": act, "path": path})) == 0
